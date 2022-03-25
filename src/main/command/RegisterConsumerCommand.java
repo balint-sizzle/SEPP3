ed6ba1db7786b6c03f795574641f0b17d17ac656
s2024006 <s2024006@ed.ac.uk>
2022-03-25 11:40:58 +0000

    Initial RegisterConsumerCommand.

--- src/main/command/RegisterConsumerCommand.java
+++ src/main/command/RegisterConsumerCommand.java
@@ -1,5 +1,24 @@
 package src.main.command;
 
-public class RegisterConsumerCommand {
-    
+import com.ibm.java.diagnostics.utils.commands.ICommand;
+
+public class RegisterConsumerCommand extends Object implements ICommand {
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String password;
+    private String paymentAccountEmail;
+
+    public void execute(Context context) {
+        boolean valid = !(this.name == null || 
+                          this.email == null || 
+                          this.phoneNumber == null || 
+                          this.password == null || 
+                          this.paymentAccountEmail == null);
+    }
+
+    public Consumer getResult() {
+        return Consumer(this.name, this.email, this.phoneNumber, this.password, this.paymentAccountEmail);
+    }
+
 }

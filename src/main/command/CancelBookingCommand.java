e1c8e7122c251024becf76d7427ea1305270b45c
ArloZhang777 <zibin@163.com>
2022-03-26 12:39:57 +0000

    change to cancelIBookingCommand

--- src/main/command/CancelBookingCommand.java
+++ src/main/command/CancelBookingCommand.java
@@ -1,5 +1,23 @@
 package src.main.command;
 
-public class CancelBookingCommand {
-    
+public class CancelBookingCommand extends Object implements ICommand{
+    public long bookingNumber;
+
+
+    public CancelBookingCommand​(long bookingNumber){
+        this.bookingNumber = bookingNumber;
+    }
+
+    @Override
+    public void execute(Context context) {
+        //todo
+//        This method should not be called directly outside of testing.
+//        Normal usage is to create a command object
+//        and execute it by passing to Controller.runCommand(ICommand) instead
+    }
+
+    @Override
+    public Object getResult() {
+        return null;
+    }
 }

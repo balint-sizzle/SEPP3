07783b837453219c298c52497b413725a0786f21
trinitygulland <gullandtrinity@gmail.com>
2022-04-07 16:19:14 +0100

    Done CancelEventCommand except for refunds. Sorted out IEventState.

--- src/main/command/CancelEventCommand.java
+++ src/main/command/CancelEventCommand.java
@@ -1,5 +1,53 @@
 package main.command;
 
+import main.controller.Context;
+import main.model.*;
+import main.state.EventState;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Date;
+
 public class CancelEventCommand {
+
+    private long eventId;
+    private String organiserMessage;
+    private Boolean successResult;
+
+    public CancelEventCommand(long eventNumber, String organiserMessage) {
+        eventId = eventNumber;
+        this.organiserMessage = organiserMessage;
+    }
+
+    public void execute(Context context) {
+        // get values of current user, event to cancel, and its performances
+        User currentUser = context.getUserState().getCurrentUser();
+        Event event = context.getEventState().findEventByNumber(eventId);
+        Collection<EventPerformance> performances = event.getPerformances();
+
+        // check if any performances have already started or ended
+        LocalDateTime now = LocalDateTime.now();
+        boolean performancesNotStarted = true;
+
+        for (EventPerformance performance : performances) {
+            if (performance.getStartDateTime().isAfter(now)) {
+                performancesNotStarted = false;
+            }
+        }
+
+        // check all conditions before cancelling event and refunding sponsorship amount
+        if ((organiserMessage.length() != 0) && (currentUser instanceof EntertainmentProvider) &&
+                (event != null) && (event.getStatus() == EventStatus.ACTIVE) &&
+                (event.getOrganiser() == currentUser) && (performancesNotStarted)){
+            event.cancel();
+            // figure out how to refund all tickets
+
+        }
+    }
+
+    public Boolean getResult() {
+        return successResult;
+    }
+
     
 }
--- src/main/state/IEventState.java
+++ src/main/state/IEventState.java
@@ -1,5 +1,23 @@
 package main.state;
 
+import main.model.*;
+
+import java.util.List;
+
 public interface IEventState {
+
+    TicketedEvent createTicketedEvent(EntertainmentProvider organiser,
+                                       String title,
+                                       EventType type,
+                                       double ticketPrice,
+                                       int numTickets);
+
+    NonTicketedEvent createNonTicketedEvent(EntertainmentProvider organiser,
+                                            String title,
+                                            EventType type);
+
+    Event findEventByNumber(long eventNumber);
+
+    List<Event> getAllEvents();
 }
 

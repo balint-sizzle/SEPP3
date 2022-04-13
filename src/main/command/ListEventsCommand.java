746af97431f166e74e83cf18055b5f8cb3d4bf8c
Balint Szilagyi <s2064308@ed.ac.uk>
2022-04-13 13:16:29 +0100

    final
    linting is broken, minor bugs may persist

--- src/main/command/ListEventsCommand.java
+++ src/main/command/ListEventsCommand.java
@@ -4,55 +4,79 @@ import main.controller.Context;
 import main.model.Event;
 import main.model.User;
 import main.model.Consumer;
+import main.model.EventStatus;
 import main.model.ConsumerPreferences;
 import main.model.EntertainmentProvider;
 
 public enum LogStatus{LIST_USER_EVENTS_SUCCESS, LIST_USER_EVENTS_NOT_LOGGED_IN}
 
 public class ListEventsCommand {
-        boolean userEventsOnly;
+    boolean userEventsOnly;
     boolean activeEventsOnly;
     LogStatus userLoggedIn;
 
-    public ListEventBookingsCommand(boolean userEventsOnly, //filter events depending on user; 
+
+    public ListEventsCommand(boolean userEventsOnly, //filter events depending on user; 
                                                             //provider: ones they created
                                                             //consumer: ones that match their preferences
-                                    boolean activeEventsOnly)
+                            boolean activeEventsOnly)
                                     {
                                         this.userEventsOnly = userEventsOnly;
                                         this.activeEventsOnly = activeEventsOnly;
-                                        
+                                        this.matches = new List();
                                     }
     
     public void execute(Context context){
         List<Event> events = context.getEventState().getAllEvents();
+        
+
         if (this.userEventsOnly){
             User user = context.getUserState();
             if (user){
-                if (user instanceof Consumer){
-                    ConsumerPreferences preferences = user.getPreferences();
-                    for (Event event : events){
+                for (Event event : events){
+                    if (user instanceof Consumer){
                         for (EventPerformance performance : event.getPerformances())
-                        {
-                            if (preferences.preferSocialDistancing == performance.hasSocialDistancing() &&
-                                preferences.preferAirFiltration == performance.hasAirFiltration() &&
-                                preferences.preferOutdoorsOnly == performance.isOutdoors() &&
-                                preferences.preferredMaxVenueSize <= performance.getMaxVenueSize() &&
-                                preferences.preferredMaxCapacity <= performance.getMaxCapacity())
+                            {
+                                ConsumerPreferences preferences = user.getPreferences();
+                                if (preferences.preferSocialDistancing == performance.hasSocialDistancing() &&
+                                    preferences.preferAirFiltration == performance.hasAirFiltration() &&
+                                    preferences.preferOutdoorsOnly == performance.isOutdoors() &&
+                                    preferences.preferredMaxVenueSize <= performance.getMaxVenueSize() &&
+                                    preferences.preferredMaxCapacity <= performance.getMaxCapacity()){
+                                    if (this.activeEventsOnly){
+                                        if (event.getStatus() == EventStatus.ACTIVE) this.matches.add(event);
+                                    }
+                                    else{
+                                        this.matches.add(event);
+                                        }
+                                    }
+                            }
+                    }
+                    if (user instanceof EntertainmentProvider){
+                        if (user.equals(event.getOrganiser())){
+                            if (this.activeEventsOnly){
+                                if (event.getStatus()==EventStatus.ACTIVE){
+                                    this.matches.add(event);
+                                }
+                            }
+                            else{
+                                this.matches.add(event);
+                            }
+
                         }
-
-
                     }
                 }
-                this.userLoggedIn = LIST_USER_EVENTS_SUCCESS;
             }
-            else{
-                this.userLoggedIn = LIST_USER_EVENTS_NOT_LOGGED_IN;
+        }
+        else{
+            for (Event event : events){
+                if (event.getStatus == EventStatus.ACTIVE) this.matches.add(event);
             }
+
         }
     }
 
     public List<Event> getResult(){
-        if (this.)
+        return this.matches;
     }
 }

1221d0426563fc52a6073a7f0500f1d2cf0f0ec2
s2024006 <s2024006@ed.ac.uk>
2022-04-13 22:40:11 +0100

    Implemented request booking records commands and their respective references.

--- src/main/command/GovernmentReport1Command.java
+++ src/main/command/GovernmentReport1Command.java
@@ -1,5 +1,52 @@
 package main.command;
 
-public class GovernmentReport1Command {
-    
+import main.controller.Context;
+import main.model.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class GovernmentReport1Command implements ICommand{
+    private final LocalDateTime intervalStartInclusive;
+    private final LocalDateTime intervalEndInclusive;
+    private List<Booking> bookingListResult;
+
+    public GovernmentReport1Command(LocalDateTime intervalStartInclusive, LocalDateTime intervalEndInclusive) {
+        this.intervalStartInclusive = intervalStartInclusive;
+        this.intervalEndInclusive = intervalEndInclusive;
+    }
+
+    public void execute(Context context) {
+        List<Event> allEvents = context.getEventState().getAllEvents();
+        for (Event event : allEvents) {
+            boolean isSponsored = ((TicketedEvent) event).isSponsored();
+            if (isSponsored && (event.getStatus() == EventStatus.ACTIVE)) {
+                List<Booking> eventBookings = context.getBookingState().findBookingsByEventNumber(event.getEventNumber());
+                for (Booking booking : eventBookings) {
+                    EventPerformance performance = booking.getEventPerformance();
+                    boolean timeInclusive = (performance.getStartDateTime().isBefore(intervalStartInclusive) &&
+                                             performance.getEndDateTime().isAfter(intervalEndInclusive)) ||
+
+                                            (performance.getStartDateTime().isBefore(intervalStartInclusive) &&
+                                             performance.getEndDateTime().isAfter(intervalStartInclusive) &&
+                                             performance.getEndDateTime().isBefore(intervalEndInclusive)) ||
+
+                                            (performance.getStartDateTime().isAfter(intervalStartInclusive) &&
+                                             performance.getStartDateTime().isBefore(intervalEndInclusive) &&
+                                             performance.getEndDateTime().isBefore(intervalEndInclusive)) ||
+
+                                            (performance.getStartDateTime().isAfter(intervalStartInclusive) &&
+                                             performance.getEndDateTime().isBefore(intervalEndInclusive));
+
+                    if (timeInclusive) {
+                        bookingListResult.add(booking);
+                    }
+                }
+            }
+        }
+    }
+
+    public List<Booking> getResult() {
+        return bookingListResult;
+    }
 }
--- src/main/model/BookingStatus.java
+++ src/main/model/BookingStatus.java
@@ -1,47 +1,52 @@
 package main.model;
 
-import javax.swing.plaf.nimbus.State;
+import main.state.IBookingState;
+
 import java.util.Arrays;
 
 enum BookingStatus {
-     Active,
+    Active,
     CancelledByConsumer,
     CancelledByProvider,
-    PaymentFailed;
-
-
-    public static BookingStatus[] values(Class<? extends Enum<?>> e) {
-        return Arrays.stream(e.getEnumConstants()).map(Enum::name).toArray(BookingStatus[]::new);
-    }
-//    public static String[] names() {
-//        State[] states = values();
-//        String[] names = new String[states.length];
-//
-//        for (int i = 0; i < states.length; i++) {
-//            names[i] = states[i].name();
-//        }
-//
-//        return names;
-//    }
-    public static BookingStatus valueOf(String name){
-//        Throws:
-//        IllegalArgumentException - if this enum type has no constant with the specified name
-//                NullPointerException - if the argument is null
-        String BookingStatus [] = {"Active",
-                "CancelledByConsumer",
-                "CancelledByProvider",
-                "PaymentFailed"};
-        if(name.equals("Active") || name.equals("CanCelledByConsumer") || name.equals("CancelledByProvider")||
-        name.equals("PaymentFailed")){
-            return null;
-        }
-        else if(name == null){
-            throw new NullPointerException("the argument can not be null");
-
-        }
-        else{
-            throw new IllegalArgumentException("should has constant with the specified name");
-        }
-    }
+    PaymentFailed
 }
+
+//    public static BookingStatus[] values(Class<? extends Enum<?>> e) {
+//        return Arrays.stream(e.getEnumConstants()).map(Enum::name).toArray(BookingStatus[]::new);
+//    }
+////    public static String[] names() {
+////        State[] states = values();
+////        String[] names = new String[states.length];
+////
+////        for (int i = 0; i < states.length; i++) {
+////            names[i] = states[i].name();
+////        }
+////
+////        return names;
+////    }
+//
+//    @Override
+//    public BookingStatus valueOf(String name){
+////        Throws:
+////        IllegalArgumentException - if this enum type has no constant with the specified name
+////                NullPointerException - if the argument is null
+//        String BookingStatus[] = {"Active",
+//                                  "CancelledByConsumer",
+//                                  "CancelledByProvider",
+//                                  "PaymentFailed"};
+//        if (name.equals("Active") ||
+//            name.equals("CanCelledByConsumer") ||
+//            name.equals("CancelledByProvider") ||
+//            name.equals("PaymentFailed")) {
+//            return null;
+//        }
+//        else if (name == null) {
+//            throw new NullPointerException("the argument can not be null");
+//
+//        }
+//        else {
+//            throw new IllegalArgumentException("should has constant with the specified name");
+//        }
+//    }
+
 

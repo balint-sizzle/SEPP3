1bd226c8eff52c9a5359132cdd5ec8ce4e26de91
trinitygulland <gullandtrinity@gmail.com>
2022-04-14 14:04:56 +0100

    Sorted UpdateProfileCommand and started on UpdateConsumerProfileCommand

--- src/main/command/UpdateProfileCommand.java
+++ src/main/command/UpdateProfileCommand.java
@@ -1,5 +1,37 @@
 
 package main.command;
-public class UpdateProfileCommand {
+
+import main.controller.Context;
+import main.model.User;
+
+import java.util.Collection;
+
+public abstract class UpdateProfileCommand implements ICommand {
+
+    protected boolean successResult;
+
+    protected boolean isProfileUpdateInvalid(Context context,
+                                             String oldPassword,
+                                             String newEmail) {
+        // check if passwords match
+        User currentUser = context.getUserState().getCurrentUser();
+
+        // check if anyone else is registered with email
+        boolean emailNotRegistered = true;
+        Collection<User> users = context.getUserState().getAllUsers().values();
+
+        for (User user : users) {
+            if (user.getEmail().equals(newEmail)) {
+                emailNotRegistered = false;
+                break;
+            }
+        }
+
+        return (currentUser.checkPasswordMatch(oldPassword) && emailNotRegistered);
+    }
+
+    public Boolean getResult() {
+        return successResult;
+    }
     
 }

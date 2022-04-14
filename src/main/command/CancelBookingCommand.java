878285748917e225f090fbee9ec264095ab7e2a0
ArloZhang777 <zibin@163.com>
2022-04-14 11:26:39 +0100

    change to book parts

--- src/main/command/CancelBookingCommand.java
+++ src/main/command/CancelBookingCommand.java
@@ -2,20 +2,6 @@ package main.command;
 
 import main.controller.Context;
 
-public class CancelBookingCommand extends Object implements ICommand{
-    long bookingNumber;
-    CancelBookingCommand(long bookingNumber){
-        this.bookingNumber = bookingNumber;
+public class CancelBookingCommand {
 
-    }
-
-    @Override
-    public boolean execute(Context context) {
-        return false;
-    }
-
-    @Override
-    public Boolean getResult() {
-        return null;
-    }
 }
--- src/main/controller/Context.java
+++ src/main/controller/Context.java
@@ -1,54 +1,7 @@
 package main.controller;
-import main.external.*;
-import main.state.*;
+
 
 public class Context {
 
-    private MockEntertainmentProviderSystem entertainmentProviderSystem;
-    private MockPaymentSystem paymentSystem;
 
-    private IUserState userState;
-    private IBookingState bookingState;
-    private IEventState eventState;
-    private ISponsorshipState sponsorshipState;
-
-    private ISponsorshipState attribute; // TODO FIGURE OUT WHAT THIS IS
-
-    public Context(){
-        this.entertainmentProviderSystem = new MockEntertainmentProviderSystem();
-        this.paymentSystem = new MockPaymentSystem();
-        this.userState = new UserState();
-        this.bookingState = new BookingState();
-        this.eventState = new EventState();
-        this.sponsorshipState = new SponsorshipState();
-    }
-
-    public Context(Context other){
-        paymentSystem = (MockPaymentSystem) other.getPaymentSystem();
-        userState = (UserState) other.getUserState();
-        eventState = (EventState) other.getEventState();
-        bookingState = (BookingState) other.getBookingState();
-        sponsorshipState = (SponsorshipState) other.getSponsorshipState();
-
-    }
-
-    public IUserState getUserState() {
-        return userState;
-    }
-
-    public IEventState getEventState() {
-        return eventState;
-    }
-
-    public IBookingState getBookingState() {
-        return bookingState;
-    }
-
-    public ISponsorshipState getSponsorshipState() {
-        return sponsorshipState;
-    }
-
-    public PaymentSystem getPaymentSystem() {
-        return paymentSystem;
-    }
 }

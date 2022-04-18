6596177e646e82fa9936512e8c1a53753d0d7f79
Balint Szilagyi <s2064308@ed.ac.uk>
2022-04-18 11:38:56 +0100

    Userstate test done

--- src/test/TestUserState.java
+++ src/test/TestUserState.java
@@ -1,17 +1,7 @@
-import main.command.*;
-import main.controller.Controller;
-import main.logging.Logger;
-import main.model.*;
+import main.model.Consumer;
+import main.model.User;
 import main.state.UserState;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInfo;
-
-import java.time.LocalDateTime;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -24,10 +14,15 @@ public class TestUserState {
 
     @Test
     void add3Users(){
-        User u1 = new User("User 1", "pwd1", "emal1");
-        User u2 = new User("User 1", "pwd1", "emal1");
-        User u3 = new User("User 1", "pwd1", "emal1");
+        User u1 = new Consumer("User 1", "email1", "1234", "pwd", "paymail1");
+        User u2 = new Consumer("User 2", "email2", "12345", "pwd2", "paymail2");
+        User u3 = new Consumer("User 3", "email3", "12346", "pwd3", "paymail3");
 
-        this.uState.addUser();
+        this.uState.addUser(u1);
+        this.uState.addUser(u2);
+        this.uState.addUser(u3);
+
+        assertEquals(null, this.uState.getCurrentUser());
+        assertEquals(3, this.uState.getAllUsers().size());
     }
 }
